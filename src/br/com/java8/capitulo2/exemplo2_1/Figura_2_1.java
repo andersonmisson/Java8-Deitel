e38cdd0ee18ba7d9032ce884db9cb8c054bab70d
Anderson Misson <missondeveloper@gmail.com>
2020-01-16 11:21:15 -0300

    Exemplo 2.1

---
+++ src/br/com/java8/capitulo2/exemplo2_1/Figura_2_1.java
@@ -0,0 +1,14 @@
+package br.com.java8.capitulo2.exemplo2_1;
+
+// Figura 2.1: Welcome Java
+// Programa De Impressão de Texto
+
+public class Figura_2_1 {
+	// Metodo main inicia a execução do aplicativo Java
+	public static void main(String[] args) {
+
+		System.out.println("Bem Vindo ao Programam Java!");
+
+	}// Fim do método main
+
+}// Fim Da Classe Figura_2_1
