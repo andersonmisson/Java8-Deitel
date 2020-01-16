cd2ae05a2988c57581d282116b3c27818774b180
Anderson Misson <missondeveloper@gmail.com>
2020-01-16 19:05:56 -0300

    Exemplo 2.6

---
+++ src/br/com/java8/capitulo2/exemplo2_6/Figura_2_6.java
@@ -0,0 +1,13 @@
+package br.com.java8.capitulo2.exemplo2_6;
+
+// Figura 2.6: Welcome4.java
+// Exibindo múltiplas linhas com o método System.out.printf.
+public class Figura_2_6 {
+	
+	//método main que inicia a execução do aplicativo em java
+	public static void main(String[] args) {
+		System.out.printf("%s%n%s%n","Bem Vindo ao", "Programa Java");
+		
+	}// fim do método main
+
+}//fim da classe Welcome4 (Figura_2_6)
