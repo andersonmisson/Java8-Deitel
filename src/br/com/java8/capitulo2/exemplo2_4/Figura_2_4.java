64204cf296eafc17859a4a167da1c47c843d57eb
Anderson Misson <missondeveloper@gmail.com>
2020-01-16 18:15:31 -0300

    Exemplo 2.4

---
+++ src/br/com/java8/capitulo2/exemplo2_4/Figura_2_4.java
@@ -0,0 +1,13 @@
+package br.com.java8.capitulo2.exemplo2_4;
+
+// Figura 2.4: Welcome3.java
+// Imprimindo múltiplas linhas de texto com uma única instrução.
+public class Figura_2_4 {
+	
+	//método main que inicia a execução do aplicativo em java
+	public static void main(String[] args) {
+		System.out.print("Bem Vindo\nao\nPrograma\nJava");
+		
+	}// fim do método main
+
+}//fim da classe Welcome3 (Figura_2_4)
