0ced72de7d74c141cb67998c305aedf260602acf
Anderson Misson <missondeveloper@gmail.com>
2020-01-17 20:12:43 -0300

    Exemplo 2.15 - Comparação

---
+++ src/br/com/java8/capitulo2/exemplo2_15/Figura_2_15.java
@@ -0,0 +1,43 @@
+package br.com.java8.capitulo2.exemplo2_15;
+
+import java.util.Scanner;
+
+// Figura 2.15: Comparação.java
+// Compara inteiros utilizando instruções IF, operadores relacionais
+// e operadores de igualdade
+
+public class Figura_2_15 {
+	// método main inicia a execução do aplicativo Java
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int numero1; // primeiro número a comparar
+		int numero2; // segundo número a comparar
+		
+		System.out.print("Entre com o primeiro número inteiro: ");
+		numero1 = input.nextInt(); // lê o primeiro número fornecido pelo usuário
+		
+		System.out.print("Entre com o segundo número inteiro: ");
+		numero2 = input.nextInt(); // lê o segundo número fornecido pelo usuário
+		
+		if (numero1 == numero2)
+			System.out.printf("%d == %d%n", numero1, numero2);
+		
+		if (numero1 != numero2)
+			System.out.printf("%d != %d%n", numero1, numero2);
+		
+		if (numero1 < numero2)
+			System.out.printf("%d < %d%n", numero1, numero2);
+		
+		if (numero1 > numero2)
+			System.out.printf("%d > %d%n", numero1, numero2);
+		
+		if (numero1 <= numero2)
+			System.out.printf("%d <= %d%n", numero1, numero2);
+		
+		if (numero1 >= numero2)
+			System.out.printf("%d >= %d%n", numero1, numero2);
+
+	} // fim do método main
+
+} // fim da classe Comparação (Figura 2.15)
