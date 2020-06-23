5c01beaf74954bf97f7e7f24b8dac94854105e62
Anderson Misson <missondeveloper@gmail.com>
2020-06-23 17:50:31 -0300

    Exercício 05.27 - O que esse programa faz.

---
+++ src/br/com/java8/capitulo05/exercicio_05_27/OQueEsseProgramaFaz.java
@@ -0,0 +1,18 @@
+package br.com.java8.capitulo05.exercicio_05_27;
+
+public class OQueEsseProgramaFaz {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++){
+			for (int j = 1; j <= 3; j++){
+				for (int k = 1; k <= 4; k++)
+					System.out.print('*');
+	      System.out.println();     
+	      } // fim do for interno
+	   System.out.println(); 
+	   } // fim do for externo
+
+
+	}
+
+}
