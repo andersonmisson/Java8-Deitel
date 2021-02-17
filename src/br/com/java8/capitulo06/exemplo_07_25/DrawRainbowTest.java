6aed55d283ba142ef831461f868c9a48682fb032
Anderson Misson <missondeveloper@gmail.com>
2021-02-17 17:47:54 -0300

    Exemplo 07.25 - GUI Desenhando um Arco-Iris

---
+++ src/br/com/java8/capitulo06/exemplo_07_25/DrawRainbowTest.java
@@ -0,0 +1,19 @@
+package br.com.java8.capitulo06.exemplo_07_25;
+
+import javax.swing.JFrame;
+
+public class DrawRainbowTest {
+
+	public static void main(String[] args) {
+		
+		DrawRainbow panel = new DrawRainbow();
+		JFrame application = new JFrame();
+		
+		application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		application.add(panel);
+		application.setSize(400, 250);
+		application.setVisible(true);
+
+	}
+
+} // Fim da Classe DrawRainbowTest
